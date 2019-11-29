a4286e6b5e3530379fe2016d71e1a42e42a58def
Nakash Sheikh <nakashsheikh@Nakashs-MacBook-Pro.local>
2019-11-29 13:11:27 -0500

    Added Entity Framework to Api Project

---
+++ MicroRabbit.Banking.Api/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using MicroRabbit.Banking.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MicroRabbit.Banking.Api
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<BankingDbContext>();
+            var connectionString = configuration.GetConnectionString("BankingDbConnection");
+            builder.UseSqlServer(connectionString);
+            return new BankingDbContext(builder.Options);
+        }
+    }
+}
