52a60b2e40ad6a75c825733afb6a0032f0fe68df
Nakash Sheikh <nakashsheikh@Nakashs-MacBook-Pro.local>
2019-11-30 19:37:10 -0500

    Added Migrations for Transfer Service

---
+++ MicroRabbit.Transfer.Api/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MicroRabbit.Transfer.Api
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TransferDbContext>
+    {
+        public TransferDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<TransferDbContext>();
+            var connectionString = configuration.GetConnectionString("TransferDbConnection");
+            builder.UseSqlServer(connectionString);
+            return new TransferDbContext(builder.Options);
+        }
+    }
+}
---
+++ MicroRabbit.Transfer.Domain/Models/TranferLog.cs
@@ -0,0 +1,14 @@
+﻿namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TranferLog
+    {
+        public int Id { get; set; }
+
+        public int FromAccount { get; set; }
+
+        public int ToAccount { get; set; }
+
+        public decimal TransferAmount { get; set; }
+
+    }
+}
