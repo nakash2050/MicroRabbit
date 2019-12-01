4d7c2c1cd3b3635779d6b0f9463e1699f2f769dc
Nakash Sheikh <nakashsheikh@Nakashs-MacBook-Pro.local>
2019-11-30 20:16:04 -0500

    Added Transfer files

---
+++ MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Application.Services
+{
+    public class TransferService : ITransferService
+    {
+        private ITransferRepository _transferRepository;
+        private IEventBus _bus;
+
+        public TransferService(ITransferRepository transferRepository, IEventBus bus)
+        {
+            _transferRepository = transferRepository;
+            _bus = bus;
+        }
+
+        public IEnumerable<TranferLog> GetTranferLogs()
+        {
+            return _transferRepository.GetTransferLogs();
+        }
+    }
+}
---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private TransferDbContext _context;
+
+        public TransferRepository(TransferDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TranferLog> GetTransferLogs()
+        {
+            return _context.TranferLogs;
+        }
+    }
+}
